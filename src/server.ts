14b8c6eb5ce695a2f490d28ac66f57655af704c4
jubayerCodes <jubayerhossain111220@gmail.com>
2025-07-17 13:20:57 +0600

    project setup completed

---
+++ src/server.ts
@@ -0,0 +1,73 @@
+/* eslint-disable no-console */
+
+import { Server } from 'http'
+import mongoose from 'mongoose';
+import app from './app';
+import { envVars } from './app/config/env';
+
+let server: Server;
+
+const startServer = async () => {
+    try {
+        await mongoose.connect(envVars.DB_URL);
+
+        console.log("Connected to DB!!");
+
+        server = app.listen(5000, () => {
+            console.log(`Server is listening to post ${envVars.PORT}`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+startServer()
+
+process.on("unhandledRejection", () => {
+    console.log("Unhandled rejection detected. Server shutting down...");
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+
+    process.exit(1)
+})
+
+
+process.on("uncaughtException", () => {
+    console.log("Uncaught exception detected. Server shutting down...");
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+
+    process.exit(1)
+})
+
+process.on("SIGTERM", () => {
+    console.log("SIGTERM signal detected. Server shutting down...");
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+
+    process.exit(1)
+})
+
+process.on("SIGINT", () => {
+    console.log("SIGINT signal detected. Server shutting down...");
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+
+    process.exit(1)
+})
